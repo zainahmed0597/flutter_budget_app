75de506faef722a0813b8679aeb631b97bc20242
zainahmed0597 <zainahmed0597@gmail.com>
2021-01-11 21:41:45 +0500

    ...

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,7 +10,45 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
-        slivers: [],
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 100.0,
+            forceElevated: true,
+            floating: true,
+            pinned: true,
+            leading: IconButton(
+              icon: Icon(
+                Icons.settings,
+                size: 30.0,
+              ),
+              onPressed: () {},
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.add,
+                  size: 30.0,
+                ),
+                onPressed: () {},
+              )
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Simple Budget'),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  margin: EdgeInsets.all(10.0),
+                  height: 100.0,
+                  color: Colors.red,
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
